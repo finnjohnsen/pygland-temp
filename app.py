99c4828735ffbe0f3e7fde7cb29685e385991145
Finn Johnsen <johnsenf@gmail.com>
2024-04-13 01:25:43 +0200

    notify OLED when BLE connected

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ class App:
         except OSError:
             print("No OLED")
             self.display = None
-        self.tempble = TempBLE(self.config["bleName"])
+        self.bleConnectionEvent = asyncio.Event()
+        self.bleConnected = False
+        self.tempble = TempBLE(self.config["bleName"], self.bleConnectionEvent)
 
     def updateOLEDWithSensing(self, th):
         if self.display == None:
@@ -28,14 +30,18 @@ class App:
         self.display.fill(0)
         self.display.text(f"{name}", 0, 24)
         self.display.text(f"{temp}", 0, 0)
-        self.display.text(f"{hum}%", 75, 0, 1)
+        if self.bleConnected:
+            self.display.text(f"B", 55, 10)
+        else:
+            self.display.text(f"-", 55, 10)
+        self.display.text(f"{hum}%", 75, 0)
         self.display.show()
 
     async def updateOLED(self):
-        await asyncio.sleep_ms(500)
+        if self.display == None:
+            return
         while True:
             th = self.sensor.get_temp_humi()
-            #print(f"New sensing '{th}'")
             self.updateOLEDWithSensing(th)
             await asyncio.sleep_ms(self.config["updateIntervalMs"]["OLED"])
 
@@ -53,15 +59,24 @@ class App:
         self.display.text('Starter...', 0, 0, 1)
         self.display.show()
 
+    async def bleConnectionWaiter(self, event):
+        while True:
+            self.bleConnected = False
+            await event.wait()
+            if event.is_set() and not self.bleConnected:
+                self.bleConnected = True
+            elif not event.is_set() and self.bleConnected:
+                self.bleConnected = False
+            await asyncio.sleep_ms(self.config["updateIntervalMs"]["OLED"])
+
     async def asyncMain(self):
         updaterOLED = asyncio.create_task(self.updateOLED())
         updaterBLECharacteristics = asyncio.create_task(self.updateBLECharacteristics())
         bleServer = asyncio.create_task(self.tempble.start())
-        await asyncio.gather(updaterOLED, updaterBLECharacteristics, bleServer)
+        bleConnectionWaiter = asyncio.create_task(self.bleConnectionWaiter(self.bleConnectionEvent))
+        await asyncio.gather(updaterOLED, updaterBLECharacteristics, bleConnectionWaiter, bleServer)
 
     def run(self):
         self.startDisplayMessage()
         asyncio.run(self.asyncMain())
-        
-
         
